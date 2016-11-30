ce6d4aa8592aa1c679241f911d68a02e6d7d995c
Dustin Walker <dwtimp@gmail.com>
2016-11-30 13:31:54 -0600

    starting register-user page

--- app/router.js
+++ app/router.js
@@ -7,6 +7,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route(`index`, {path: `/` }, function() {
+
+    this.route('register-user');
+  });
+
 });
 
 export default Router;
