bcb217de7e3814cbf59729ad22c2d80ab4922ab5
Isaac Weber <ikey2244@gmail.com>
2016-12-07 16:43:34 -0600

    creating some stuff for tabs

---
+++ app/coffee-shop-account/controller.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  tabs: [
+    {
+      name: 'Account',
+      route: 'coffee-shop-account.account.index',
+      icon: 'fa fa-user'
+    },
+    {
+      name: 'Menu',
+      route: 'coffee-shop-account.menu.index',
+      icon: 'fa fa-cutlery'
+    },
+    {
+      name: 'Incoming orders',
+      route: 'coffee-shop-account.incoming-orders',
+      icon: 'fa fa-tasks'
+    },
+  ],
+});
