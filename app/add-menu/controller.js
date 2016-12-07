c9545c16eb7220c43c82cca09dbfc1879d265f62
Dustin Walker <dwtimp@gmail.com>
2016-12-07 15:24:40 -0600

    added simple-form to checkout-page

--- app/add-menu/controller.js
+++ app/add-menu/controller.js
@@ -2,15 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  sizes: ['small', 'medium', 'large'],
+  sizes: [
+    'small', 'medium', 'large'
+  ],
 
   actions: {
-    addItem() {
-
+    addItem(formValues) {
+      const item = this.store.createRecord('item', formValues);
+      item.set('size', this.size);
+      item.save().then(() => {
+        this.transitionToRoute('menu-items');
+      }).catch(() => {
+        alert('Error creating menu item');
+      });
     },
-    chooseSize(sizes) {
-      console.log(sizes)
-      this.set(sizes);
+    chooseSize(size) {
+      this.set('size', size);
     }
   }
 });
