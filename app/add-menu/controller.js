8232e4cd6d278d72d94b70ad973e2a6bfc91e848
Dustin Walker <dwtimp@gmail.com>
2016-12-07 10:50:48 -0600

    added simple form, power select to add-menu

--- app/add-menu/controller.js
+++ app/add-menu/controller.js
@@ -5,7 +5,11 @@ export default Ember.Controller.extend({
   sizes: ['small', 'medium', 'large'],
 
   actions: {
+    addItem() {
+
+    },
     chooseSize(sizes) {
+      console.log(sizes)
       this.set(sizes);
     }
   }
