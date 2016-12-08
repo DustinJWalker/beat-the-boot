b9f63f670d441697ed7d68544c2b62e8eb57902b
Dustin Walker <dwtimp@gmail.com>
2016-12-08 11:21:20 -0600

    working on controller for menu-items

--- app/menu-items/controller.js
+++ app/menu-items/controller.js
@@ -3,4 +3,25 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   session: Ember.inject.service(),
 
+  quantities: [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+  ],
+
+  actions: {
+    addDrink(formValues) {
+      const drink = this.store.createRecord('drink', formValues);
+      drink.set('quantity', this.quantity);
+      drink.save().then(() => {
+        this.transitionToRoute('checkout-page');
+      }).catch(() => {
+        alert('Error creating menu item');
+      });
+    },
+    selectQuantity(size) {
+      this.set('size', size);
+    }
+  }
+
+
+
 });
