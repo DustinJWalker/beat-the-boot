aecdc9a92fb5f00adcf2ed78f860b5e61397f30b
Isaac Weber <ikey2244@gmail.com>
2016-12-12 16:57:57 -0600

    displaying menu data and totaling prices

---
+++ tests/unit/helpers/price-total-test.js
@@ -0,0 +1,12 @@
+
+import { priceTotal } from 'beat-the-boot/helpers/price-total';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | price total');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = priceTotal([42]);
+  assert.ok(result);
+});
+
