795100e2ba25aa0b4c5948e82e3fc0eeee76983a
Dustin Walker <dwtimp@gmail.com>
2016-12-12 10:33:27 -0600

    created order model

--- app/models/order.js
+++ app/models/order.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   belongsTo: DS.attr('shop'),
   lineItems: DS.hasMany('line-item'),
-  drink: DS.attr('str'),
+  drink: DS.attr('string'),
   price: DS.attr('number')
 });
